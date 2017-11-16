3161c85d806b20074298586474852e03fa8e9536
hashrock <hashrock@users.noreply.github.com>
2017-11-17 01:27:40 +0900

    refactor: export draw functions

--- app-logic.ts
+++ app-logic.ts
@@ -2,7 +2,7 @@ import * as request from "superagent";
 import * as parser from "./lib/lff/parser";
 import { Line } from "./lib/lff/parser";
 import * as example from "./example"
-
+import {clear, drawChar, drawString} from "./draw"
 export default {
   data(){
     return {
@@ -18,83 +18,13 @@ export default {
       return "rgba(" + r + ", " + g + ", " + b + "," + a + ")";
     }
 
-    function aline(x: number, y: number, x1: number, y1: number, x2: number, y2: number, scale: number, option: LffOption, index: number) {
-      line(
-        x1 * scale + x,
-        (10 - y1) * scale + y,
-        x2 * scale + x,
-        (10 - y2) * scale + y,
-        option,
-        index
-      );
-    }
-
-    function line(x1: number, y1: number, x2: number, y2: number, option: LffOption, index: number) {
-      if (option.lineRenderer) {
-        option.lineRenderer(ctx, round, count, x1, y1, x2, y2, index);
-      } else {
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-      }
-    }
-
-    function drawChar(x: number, y: number, size: number, fontdata: parser.Font[], str: string, option: LffOption) {
-      var font;
-      for (var i = 0; i < fontdata.length; i++) {
-        if (fontdata[i].letter === str) {
-          font = fontdata[i];
-          break;
-        }
-      }
-
-      if (font) {
-        ctx.lineWidth = 0.5;
-        font.info.forEach(function (stroke: parser.Line, index: number) {
-          aline(
-            x,
-            y,
-            stroke.x1,
-            stroke.y1,
-            stroke.x2,
-            stroke.y2,
-            size,
-            option,
-            index
-          );
-        });
-      }
-    }
-
-    function drawString(x: number, y: number, size: number, fontdata: parser.Font[], str: string, option: LffOption) {
-      var ratio = size / 10;
-
-      option = option ? option : { lineRenderer: () => { } };
-
-      var offset = 0;
-      for (var i = 0; i < str.length; i++) {
-        var ch = str[i];
-        drawChar(x + offset, y, ratio, fontdata, ch, option);
-        if (str.match(/^(\w| |'|,|&)+$/)) {
-          offset += ratio * 5;
-        } else {
-          offset += ratio * 10;
-        }
-      }
-    }
-
-    function clear() {
-      ctx.clearRect(0, 0, w, h);
-    }
-
     var fontdata: parser.Font[];
     var round = 0;
     var count = 0;
     function update() {
       round += 0.01;
       count += 1;
-      clear();
+      clear(ctx, w, h);
       if (fontdata) {
         var left = 20;
         var top = 20;
@@ -104,13 +34,13 @@ export default {
         var customRenderer2 = example.custom2
         var size = 80;
 
-        drawString(left, top, size, fontdata, "あいうえお", {
+        drawString(ctx, round, count, left, top, size, fontdata, "あいうえお", {
           lineRenderer: customRenderer
         });
-        drawString(left, top + size, size, fontdata, "ABCDE123", {
+        drawString(ctx, round, count, left, top + size, size, fontdata, "ABCDE123", {
           lineRenderer: customRenderer
         });
-        drawString(left, top + size * 2, size / 2, fontdata, "漢字も描画可能", {
+        drawString(ctx, round, count, left, top + size * 2, size / 2, fontdata, "漢字も描画可能", {
           lineRenderer: customRenderer2
         });
       }
@@ -127,6 +57,3 @@ export default {
   }
 };
 
-class LffOption {
-  lineRenderer: (ctx: CanvasRenderingContext2D, round: number, count: number, x0: number, y0: number, x1: number, y1: number, index: number) => void;
-}
---
+++ draw.ts
@@ -0,0 +1,82 @@
+import * as request from "superagent";
+import * as parser from "./lib/lff/parser";
+import { Line } from "./lib/lff/parser";
+import * as example from "./example"
+class LffOption {
+  lineRenderer: (ctx: CanvasRenderingContext2D, round: number, count: number, x0: number, y0: number, x1: number, y1: number, index: number) => void;
+}
+export function aline(ctx: CanvasRenderingContext2D, round: number, count: number, x: number, y: number, x1: number, y1: number, x2: number, y2: number, scale: number, option: LffOption, index: number) {
+  line(
+    ctx,
+    round,
+    count,
+    x1 * scale + x,
+    (10 - y1) * scale + y,
+    x2 * scale + x,
+    (10 - y2) * scale + y,
+    option,
+    index
+  );
+}
+
+export function line(ctx: CanvasRenderingContext2D, round: number, count: number, x1: number, y1: number, x2: number, y2: number, option: LffOption, index: number) {
+  if (option.lineRenderer) {
+    option.lineRenderer(ctx, round, count, x1, y1, x2, y2, index);
+  } else {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+  }
+}
+
+export function drawChar(ctx: CanvasRenderingContext2D, round:number, count:number, x: number, y: number, size: number, fontdata: parser.Font[], str: string, option: LffOption) {
+  var font;
+  for (var i = 0; i < fontdata.length; i++) {
+    if (fontdata[i].letter === str) {
+      font = fontdata[i];
+      break;
+    }
+  }
+
+  if (font) {
+    ctx.lineWidth = 0.5;
+    font.info.forEach((stroke: parser.Line, index: number) => {
+      aline(
+        ctx,
+        round,
+        count,
+        x,
+        y,
+        stroke.x1,
+        stroke.y1,
+        stroke.x2,
+        stroke.y2,
+        size,
+        option,
+        index
+      );
+    });
+  }
+}
+
+export function drawString(ctx: CanvasRenderingContext2D, round: number, count: number, x: number, y: number, size: number, fontdata: parser.Font[], str: string, option: LffOption) {
+  var ratio = size / 10;
+
+  option = option ? option : { lineRenderer: () => { } };
+
+  var offset = 0;
+  for (var i = 0; i < str.length; i++) {
+    var ch = str[i];
+    drawChar(ctx, round, count, x + offset, y, ratio, fontdata, ch, option);
+    if (str.match(/^(\w| |'|,|&)+$/)) {
+      offset += ratio * 5;
+    } else {
+      offset += ratio * 10;
+    }
+  }
+}
+
+export function clear(ctx: CanvasRenderingContext2D, w: number, h: number) {
+  ctx.clearRect(0, 0, w, h);
+}

1a0e0127f191e3c9404b3d29a9a5e0a707d7bd26
hashrock <hashrock@users.noreply.github.com>
2017-11-17 01:40:21 +0900

    refactor: move logic into sfc

--- App.vue
+++ App.vue
@@ -18,6 +18,78 @@ body {
 }
 </style>
 
-<script lang="ts" src="./app-logic">
+<script lang="ts">
+import * as parser from "./lib/lff/parser";
+import { Line } from "./lib/lff/parser";
+import * as example from "./example";
+import { clear, drawChar, drawString } from "./lib/drawfont";
+export default {
+  async mounted() {
+    const c = <HTMLCanvasElement>document.getElementById("canvas");
+    const ctx = <CanvasRenderingContext2D>c.getContext("2d");
+    const w = c.offsetWidth;
+    const h = c.offsetHeight;
+
+    function rgba(r: number, g: number, b: number, a: number) {
+      return "rgba(" + r + ", " + g + ", " + b + "," + a + ")";
+    }
+
+    let fontdata: parser.Font[] = [];
+    let round = 0;
+    let count = 0;
+    function update() {
+      round += 0.01;
+      count += 1;
+      clear(ctx, w, h);
+      if (fontdata) {
+        const left = 20;
+        const top = 20;
+
+        const customRenderer = example.custom1;
+
+        const customRenderer2 = example.custom2;
+        const size = 80;
+
+        drawString(ctx, round, count, left, top, size, fontdata, "あいうえお", {
+          lineRenderer: customRenderer
+        });
+        drawString(
+          ctx,
+          round,
+          count,
+          left,
+          top + size,
+          size,
+          fontdata,
+          "ABCDE123",
+          {
+            lineRenderer: customRenderer
+          }
+        );
+        drawString(
+          ctx,
+          round,
+          count,
+          left,
+          top + size * 2,
+          size / 2,
+          fontdata,
+          "漢字も描画可能",
+          {
+            lineRenderer: customRenderer2
+          }
+        );
+      }
+
+      window.requestAnimationFrame(() => {
+        update();
+      });
+    }
+    const response = await fetch("kst32b.lff");
+    const text = await response.text();
+    fontdata = parser.parseLines(text.split("\n"));
+    update();
+  }
+};
 </script>
 

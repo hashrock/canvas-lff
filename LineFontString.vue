ccdda986df95c28313a50ecc172b83d44485fa5b
hashrock <hashrock@users.noreply.github.com>
2017-11-19 20:34:00 +0900

    fix: Fix Letter width of hankaku characters

--- LineFontString.vue
+++ LineFontString.vue
@@ -1,6 +1,6 @@
 <template>
   <g :transform="groupPosition">
-    <line-font-char v-for="(char, index) in strAry" :key="index" :fontdata="fontdata" :text="char" :x="index * (fontSize - kerning)" :y="0" :font-size="fontSize"></line-font-char>
+    <line-font-char v-for="(char, index) in strAry" :key="index" :fontdata="fontdata" :text="char.letter" :x="posX(char.offset)" :y="0" :font-size="fontSize"></line-font-char>
   </g>
 </template>
 
@@ -8,6 +8,14 @@
 import Vue from "vue";
 import LineFontChar from "./LineFontChar.vue";
 import * as parser from "./lib/lff/parser";
+
+interface Letter{
+  letter: string
+  offset: number
+}
+function isHankaku(str: string) {
+  return str.match(/^(\w| |'|,|&)+$/);
+}
 
 export default Vue.extend({
   props: {
@@ -20,14 +28,25 @@ export default Vue.extend({
   },
   computed: {
     strAry() {
-      let strAry: string[] = [];
+      let strAry: Letter[] = [];
+      let offset : number = 0;
       for (let i = 0; i < this.str.length; i++) {
-        strAry.push(this.str.charAt(i));
+        const letter = this.str.charAt(i)
+        strAry.push({
+          letter: letter,
+          offset: offset
+        });
+        offset += isHankaku(letter) ? 0.5 : 1
       }
       return strAry;
     },
     groupPosition(): string {
       return `translate(${this.x}, ${this.y})`;
+    }
+  },
+  methods: {
+    posX(offset: number){
+      return offset * (this.fontSize - this.kerning)
     }
   },
   components: {
